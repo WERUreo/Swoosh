8afb6c85875d5bc32c8a2a9a9ad0f1684fe3408f
Keli'i Martin <kelii.d.martin@gmail.com>
2018-02-18 16:09:16 -0500

    lesson 9

--- app/src/main/java/com/werureo/swoosh/controller/FinishActivity.kt
+++ app/src/main/java/com/werureo/swoosh/controller/FinishActivity.kt
@@ -1,10 +1,10 @@
 package com.werureo.swoosh.controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.werureo.swoosh.R
-import com.werureo.swoosh.utilities.EXTRA_LEAGUE
-import com.werureo.swoosh.utilities.EXTRA_SKILL
+import com.werureo.swoosh.model.Player
+import com.werureo.swoosh.utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -13,9 +13,8 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
--- app/src/main/java/com/werureo/swoosh/controller/LeagueActivity.kt
+++ app/src/main/java/com/werureo/swoosh/controller/LeagueActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.werureo.swoosh.utilities.EXTRA_LEAGUE
 import com.werureo.swoosh.R
+import com.werureo.swoosh.model.Player
+import com.werureo.swoosh.utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,27 +22,27 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
     }
 
     fun onCoedClicked(view: View) {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View) {
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillIntent = Intent(this, SkillActivity::class.java)
-            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillIntent.putExtra(EXTRA_PLAYER, player)
             startActivity(skillIntent)
         } else {
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
